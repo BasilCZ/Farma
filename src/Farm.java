2ac0c2ca9e6f5cbee000fb864cbd464a32dc6a96
BasilCZ <ondrej.kucera@proprius.cz>
2025-01-30 09:55:35 +0100

    Odebrani printu testu commitu a pridani potrebnych metod

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Farm {
+    private ArrayList<Flower> farm = new ArrayList<>();
+    private ArrayList<Animal> barn = new ArrayList<>();
+    private double usedArea;
 
+    public Farm() {
+        this.usedArea = 0;
+    }
 
+    public void plantFlower(){}
 
+    public void harvestFlower(){}
+
+    public void waterFlower(){}
+
+    public void destroyFlower(){}
+
+    public void addAnimal(){}
+
+    public void killAnimal(){}
 }
--- src/Flower.java
+++ src/Flower.java
@@ -5,6 +5,19 @@ public abstract class Flower {
     protected double neededArea;
     protected double chanceOfGrowth;
 
+    public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        if(chanceOfGrowth>100){
+            this.chanceOfGrowth = 100;
+        } else if (chanceOfGrowth<0){
+            this.chanceOfGrowth = 0;
+        } else {
+            this.chanceOfGrowth = chanceOfGrowth;
+        }
+    }
+
     @Override
     public String toString() {
         return "Flower{" +
